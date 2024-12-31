7da9f834767bc2d4247b8c263ebe62d7edbd9f07
thimmeswarnaidu <thimmeswarnaidu1107@gmail.com>
2024-12-31 17:39:50 +0530

    Making Microservices Resilient using Resilience4j

--- gatewayserver/src/main/java/com/eazybyte/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybyte/gatewayserver/GatewayserverApplication.java
@@ -1,10 +1,21 @@
 package com.eazybyte.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
+
+import java.beans.Customizer;
+import java.time.Duration;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -23,11 +34,32 @@ public class GatewayserverApplication {
 										)
 						.uri("lb://ACCOUNTS"))
 				.route(r -> r.path("/eazybyte/loans/**")
-						.filters(f -> f.rewritePath("/eazybyte/loans/(?<segment>.*)", "/${segment}"))
+						.filters(f -> f.rewritePath("/eazybyte/loans/(?<segment>.*)", "/${segment}")
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
+
 						.uri("lb://LOANS"))
 				.route(r -> r.path("/eazybyte/cards/**")
-						.filters(f -> f.rewritePath("/eazybyte/cards/(?<segment>.*)", "/${segment}"))
+						.filters(f -> f.rewritePath("/eazybyte/cards/(?<segment>.*)", "/${segment}")
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
+
 						.uri("lb://CARDS")).build();
 
 	}
+
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
+	}
+
+
 }
